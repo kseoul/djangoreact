bbb4965f7895e5f2094f922be841bee857ac972a
kseoul <jameshseo@gmail.com>
2021-10-29 22:12:25 -0400

    Update settings.py

--- backend/djreact/djreact/settings.py
+++ backend/djreact/djreact/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0y1o&dvc2ko$vklfdll*lz1)gysshucudyhepoy8s11a_^uj4t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
